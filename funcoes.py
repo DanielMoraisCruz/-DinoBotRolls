254e2fd05287ee458c8d40a0a20c94549392263d
Daniel Cruz <49539664+DanielMoraisCruz@users.noreply.github.com>
2022-03-03 18:19:54 +0000

    tudo feito

--- funcoes.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def rolar_dados():
-    
-    return random.randrange(1, 20, 1)
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import os
-import discord
-import funcoes
-
+import os, discord
 from discord.ext import commands
-my_secret = os.environ['TOKEN']
+
+
 bot = commands.Bot("!!")
 
 @bot.event
 async def on_ready():
-    print('Fizemos login como {0.user}'.format(bot.user))
+    print('Fizemos login como {0}'.format(bot.user))
 
-bot.run(os.getenv(my_secret))
+@bot.command(name = "oi")
+async def mandar_oi(ctx):
+    name = ctx.author.name
+
+    resposta = "ola, " + name
+
+    await ctx.send(resposta)
+
+
+
+
+
+
+bot.run(os.environ['TOKEN'])
